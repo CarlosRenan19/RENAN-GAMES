ecbd4f3f392a3fc1a1c431e6c9462ae33b3c5aa3
Renan <111693987+Renanzim12@users.noreply.github.com>
2022-10-03 13:31:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle("active");
     linksmobile.classList.toggle("active");
 })
+
+var i = setInterval(function () {
+    clearInterval(i)
+
+    document.getElementById("loader-back").style.display = "none";
+    document.getElementById("toda-pagina").style.display = "block";
+}, 4000)
